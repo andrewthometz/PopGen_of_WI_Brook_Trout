3ac5d269914847ff1f6e2441b745a1fa93e9b6bb
Andrew Thometz <148142545+andrewthometz@users.noreply.github.com>
2025-05-08 12:14:52 -0500

    Update and rename Map_WI_HUC4s.R to Drifless_Area_Map.R

--- Drifless_Area_Map.R
+++ Drifless_Area_Map.R
@@ -1,8 +1,8 @@
+# Load packages
 library(tidyverse)
 library(readxl)
 library(adegenet)
 
-#### Mapping ####
 library(sf)
 library(ggrepel)
 library(ggspatial)
@@ -13,7 +13,11 @@ library(ggOceanMaps)
 
 library(ggmap)
 
-#### Grab lats longs from Samples_2205 ####
+#########################################################################################
+#### Produce a map that displays the HUC 4 subregions and Drifless Area of Wisconsin ####
+#########################################################################################
+
+# Grab lats/long coordinates from Samples_2205
 Samples_2205 <- read_delim("X:/2205_BKT_feral_broodstock_ID/Thometz_scripts/Samples_2205.csv") %>% 
   select(WaterbodyName, HUC_4, HUC_8, HUC_12, Latitude, Longitude, WBIC) %>% 
   distinct()
@@ -27,10 +31,12 @@ HUC2_shp <- read_sf("X:/2205_BKT_feral_broodstock_ID/Mapping_shapefiles/Major_Ba
 
 DriftlessArea <- read_sf("X:/2205_BKT_feral_broodstock_ID/Mapping_shapefiles/FHP_DARE_Boundary_2013.shp/FHP_DARE_Boundary_2013.shp")
 
+# Clip the Driftless Area shapefile to only show within Wisconsin
 DA_clipped <- clip_shapefile(DriftlessArea,
                              limits = HUC2_shp,
                              return.boundary = FALSE)
 
+# Read in HUC 4 shapefile and clip to only show within Wisconsin
 HUC4_shp <- read_sf("X:/2205_BKT_feral_broodstock_ID/Mapping_shapefiles/NHD_H_Wisconsin_State_Shape/Shape/WBDHU4.shp") %>% 
   filter(name %in% Samples_2205$HUC_4)
 
@@ -38,7 +44,7 @@ HUC4_clipped <- clip_shapefile(HUC4_shp,
                                limits = HUC2_shp,
                                return.boundary = TRUE) # Select TRUE to retain metadata, must subset later with $
 
-##############
+#### Produce the map ####
 temp_metadata <- Samples_2205 %>% 
   distinct()
 
@@ -104,7 +110,6 @@ HUC_DA_map <- HUC4_clipped$shapefile %>%
         panel.grid.minor = element_blank(),
         plot.background = element_blank())
   
-
 ggsave(filename = "HUCs_Driftless_map.pdf",
        plot = HUC_DA_map,
        device = "pdf",
