20ad1eed392098028e1b3cd4750a27930207690a
Andrew Thometz <148142545+andrewthometz@users.noreply.github.com>
2025-05-22 12:11:54 -0500

    Update and rename PCA_comparison.R to PCA_Amplicon_vs_CE_Comparison.R

--- PCA_Amplicon_vs_CE_Comparison.R
+++ PCA_Amplicon_vs_CE_Comparison.R
@@ -1,3 +1,4 @@
+# Load packages
 library(tidyverse)
 library(readxl)
 library(adegenet)
@@ -10,7 +11,11 @@ library(BiocManager)
 #BiocManager::install("YuLab-SMU/treedataverse")
 library(treedataverse)
 
-#### Read in Amplicon genepop file ####
+########################################################################################################################
+#### Run principle component analyses using amplicon data and capillary electrophoresis data to compare the results ####
+########################################################################################################################
+
+# Read in amplicon genepop file
 Amp_gen <- read.genepop("X:/2205_BKT_feral_broodstock_ID/Thometz_scripts/CE_vs_Amp/Amplicon_genepop.gen", 
                         ncode = 3L, 
                         quiet = FALSE)
@@ -25,12 +30,12 @@ Amp_pop_data <- read_excel("X:/2201_BKT_msat_conversion/samplesForCEComparison.x
 # Fill pop slot
 Amp_gen@pop <- as_factor(Amp_pop_data$WaterbodyName)
 
-#### Read in data ####
+# Read in metadata
 Data_2111 <- read.genepop("X:/2111_F1F2D_BKT/2111analysis/Thometz_scripts/2111_genepop.gen", 
                           ncode = 3L, 
                           quiet = FALSE)
 
-# Read in 2111 metadata
+# Read in 2111 project metadata
 Samples_2111 <- read_delim("X:/2111_F1F2D_BKT/2111analysis/Thometz_scripts/Samples_2111.csv") %>% 
   arrange(Cohort) %>% 
   filter(SampleID %in% rownames(Data_2111@tab)) %>% 
@@ -38,33 +43,33 @@ Samples_2111 <- read_delim("X:/2111_F1F2D_BKT/2111analysis/Thometz_scripts/Sampl
   mutate(WaterbodyName = case_when(str_detect(WaterbodyName, "St. Croix") ~ "Hatchery",
                                    .default = WaterbodyName))
 
-# Fill 2111 pop slot and subset to just domestic fish
+# Fill the 2111 project pop slot and subset to just domestic strain fish
 Data_2111@pop <- as_factor(Samples_2111$WaterbodyName)
 
 Domestics <- popsub(Data_2111, 
                     sublist = "Hatchery",
                     drop = FALSE)
 
-# Filter to CE vs Amp loci
+# Filter to CE vs Amplicon loci
 Domestics <- Domestics[loc = locNames(Amp_gen)]
 
-# Read in 2205 genetic data for reference comparisons
+# Read in 2205 project genetic data for reference comparisons
 Data_2205 <- read.genepop("X:/2205_BKT_feral_broodstock_ID/Thometz_scripts/2205_genepop.gen", 
                           ncode = 3L, 
                           quiet = FALSE)
 
-# # Filter to CE vs Amp loci
-# temp_gen <- Data_2205[loc = locNames(Amp_gen)]
-# 
-# # Write and read it back in to work around repooling error
-# temp_gen %>% 
-#   tidy_genind() %>% 
-#   write_genepop(genepop.header = "59 Survey pops with just 7 CE vs Amp loci",
-#                 filename = "X:/2205_BKT_feral_broodstock_ID/Thometz_scripts/Erdman_integration/2205_pops_7_loci")
+# Filter to CE vs Amp loci
+temp_gen <- Data_2205[loc = locNames(Amp_gen)]
+ 
+# Write and read it back in to work around repooling error
+temp_gen %>% 
+   tidy_genind() %>% 
+   write_genepop(genepop.header = "59 Survey pops with just 7 CE vs Amp loci",
+                 filename = "X:/2205_BKT_feral_broodstock_ID/Thometz_scripts/Erdman_integration/2205_pops_7_loci")
 
-#Data_2205_7_loci <- read.genepop("X:/2205_BKT_feral_broodstock_ID/Thometz_scripts/CE_vs_Amp/2205_pops_7_loci.gen", 
-#                                 ncode = 3L, 
-#                                 quiet = FALSE)
+Data_2205_7_loci <- read.genepop("X:/2205_BKT_feral_broodstock_ID/Thometz_scripts/CE_vs_Amp/2205_pops_7_loci.gen", 
+                                 ncode = 3L, 
+                                 quiet = FALSE)
 
 # Read in project metadata
 Samples_2205 <- read_delim("X:/2205_BKT_feral_broodstock_ID/Thometz_scripts/Samples_2205.csv") %>% 
@@ -77,13 +82,13 @@ Data_2205_7_loci@pop <- as_factor(Samples_2205$WaterbodyName)
 # Repool
 All_dat_amplicon <- repool(Data_2205_7_loci, Domestics, Amp_gen)
 
-# Create df with all SampleID's and Waterbodies
+# Create dataframe with all SampleID's and Waterbodies
 centroid_df_amp <- Samples_2111 %>% 
   bind_rows(Samples_2111) %>% 
   bind_rows(Amp_pop_data) %>% 
   select(SampleID, WaterbodyName)
 
-#### Run PCA with amplicon data ####
+#### Run PCA using just amplicon data ####
 pca_amp_data <- tab(All_dat_amplicon, freq = TRUE, NA.method = "mean")
 
 pca_amp_result <- dudi.pca(pca_amp_data, center = TRUE, scale = FALSE, nf = 4, scannf = FALSE)
@@ -96,7 +101,7 @@ pca_df_1 <- pca_amp_result$li %>%
   as_tibble() %>% 
   mutate(Population = pop_names_1$pop)
 
-# Plot
+# Plot the results
 centroids_pop_1 <- pca_df_1 %>% 
   select(Population) %>% 
   right_join(aggregate(cbind(Axis1, Axis2) ~ Population, pca_df_1, mean)) %>% 
@@ -128,9 +133,8 @@ ggsave(filename = "PCA_amplicon.pdf",
        width = 12,
        units = "in")
 
-######################## Do this again with converted CE data ##################################
-
-#### Read in converted CE genepop file ####
+#### Run PCA with just converted capillary electrophoresis data ####
+# Read in converted CE genepop file
 CE_converted <- read.genepop("X:/2205_BKT_feral_broodstock_ID/Thometz_scripts/CE_vs_Amp/CE_genepop_converted.gen", 
                              ncode = 3L, 
                              quiet = FALSE)
@@ -142,19 +146,19 @@ CE_pop_data <- read_excel("X:/2201_BKT_msat_conversion/samplesForCEComparison.xl
   rename(WaterbodyName = Location) %>% 
   arrange(match(SampleID, rownames(CE_converted@tab)))
 
-# Fill 2111 pop slot and subset to just domestic fish
+# Fill 2111 project pop slot and subset to just domestic strain fish
 CE_converted@pop <- as_factor(CE_pop_data$WaterbodyName)
 
 # Repool
 All_dat_converted <- repool(Data_2205_7_loci, Domestics, CE_converted)
 
-# Create df with all SampleID's and Waterbodies
+# Create dataframe with all SampleID's and Waterbodies
 centroid_df_CE <- Samples_2111 %>% 
   bind_rows(Samples_2111) %>% 
   bind_rows(CE_pop_data) %>% 
   select(SampleID, WaterbodyName)
 
-#### Run PCA with amplicon data ####
+# Run PCA with just capillary electrophoresis data
 pca_CE_data <- tab(All_dat_converted, freq = TRUE, NA.method = "mean")
 
 pca_CE_result <- dudi.pca(pca_CE_data, center = TRUE, scale = FALSE, nf = 4, scannf = FALSE)
@@ -167,7 +171,7 @@ pca_df_2 <- pca_CE_result$li %>%
   as_tibble() %>% 
   mutate(Population = pop_names_2$pop)
 
-# Plot
+# Plot the result
 centroids_pop_2 <- pca_df_2 %>% 
   select(Population) %>% 
   right_join(aggregate(cbind(Axis1, Axis2) ~ Population, pca_df_2, mean)) %>% 
