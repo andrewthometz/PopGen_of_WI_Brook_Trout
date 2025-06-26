5c2d5aae21b44766246c7869c723cae1b54e352b
Andrew Thometz <148142545+andrewthometz@users.noreply.github.com>
2025-06-26 12:30:44 -0500

    Update Sample_Metadata.R

--- Sample_Metadata.R
+++ Sample_Metadata.R
@@ -17,15 +17,15 @@ library(MazamaSpatialUtils)
 ######################################################################################
 
 #### Reading in MCGL Data ####
-MCGL2 <- read_excel("Z:/MCGL Database/Working/MCGL Sample Database (11-00001 to 17-15647).xlsx", 
+MCGL2 <- read_excel("Z:/filepath.../MCGL Database/Working/MCGL Sample Database (11-00001 to 17-15647).xlsx", 
                     guess_max = 1000000) %>% 
   select(SampleID, PlateID, ProjectID, CommonName, YearCollected, WaterbodyName, State, County, Collectors, Latitude, Longitude, Comments)
   
-MCGL3 <- read_excel("Z:/MCGL Database/Working/MCGL Sample Database (18-00001 to 19-28271).xlsx",
+MCGL3 <- read_excel("Z:/filepath.../MCGL Database/Working/MCGL Sample Database (18-00001 to 19-28271).xlsx",
                     guess_max = 1000000) %>% 
   select(SampleID, PlateID, ProjectID, CommonName, YearCollected, WaterbodyName, State, County, Collectors, Latitude, Longitude, Comments)
 
-MCGL4 <- read_excel("Z:/MCGL Database/Working/MCGL Sample Database (21-00001 to 22-19350).xlsx",
+MCGL4 <- read_excel("Z:/filepath.../MCGL Database/Working/MCGL Sample Database (21-00001 to 22-19350).xlsx",
                     guess_max = 1000000) %>% 
   select(SampleID, PlateID, ProjectID, CommonName, YearCollected, WaterbodyName, State, County, Collectors, Latitude, Longitude, Comments) %>% 
   mutate(YearCollected = as.numeric(YearCollected))
@@ -57,7 +57,7 @@ huc8Dat <- loadSpatialData('WBDHU8')
 huc10Dat <- loadSpatialData('WBDHU10')
 
 # HUC 12 data
-HUC12_WBICs <- read_delim("X:/2205_BKT_feral_broodstock_ID/Statewide BKT info/HUC12_WBIC.txt") %>% 
+HUC12_WBICs <- read_delim("X:/filepath.../Statewide BKT info/HUC12_WBIC.txt") %>% 
   rename(WBIC = RIVER_SY_1,
          HUC_12 = HUC12_NAME) %>% 
   filter(WBIC != 0) %>% 
@@ -96,20 +96,20 @@ Samples_2205 <- temporary %>%
 write_csv(Samples_2205, "X:/2205_BKT_feral_broodstock_ID/Thometz_scripts/Samples_2205.csv")
 
 #### Create reusable stocking data CSV ####
-Stocking_data <- read_excel("X:/2205_BKT_feral_broodstock_ID/Statewide BKT info/WI_BKT_Stocking&CPUE.xlsx", 
+Stocking_data <- read_excel("X:/filepath.../Statewide BKT info/WI_BKT_Stocking&CPUE.xlsx", 
                             sheet = "Stocking data") %>% 
   select(WBIC, WaterbodyName, Stocking_Year, n_stocked, Strain, Stock_Source_Group) %>% 
   rename(Name = WaterbodyName) %>% 
   left_join(HUC12_WBICs, relationship = "many-to-many") %>% 
   filter(HUC_12 %in% Samples_2205$HUC_12)
 
-write_csv(Stocking_data, "X:/2205_BKT_feral_broodstock_ID/Thometz_scripts/Stocking_histories_2205.csv")
+write_csv(Stocking_data, "X:/filepath.../Stocking_histories_2205.csv")
 
 #### Create reusable CPE data CSV ####
-CPE_data <- read_excel("X:/2205_BKT_feral_broodstock_ID/Statewide BKT info/WI_BKT_Stocking&CPUE.xlsx", 
+CPE_data <- read_excel("X:/filepath.../Statewide BKT info/WI_BKT_Stocking&CPUE.xlsx", 
                        sheet = ">200mile") %>% 
   select(WBIC, Survey_Year, Hours, N_Fish, CPE_Hour) %>% 
   rename(n_caught = N_Fish) %>% 
   filter(WBIC %in% Samples_2205$WBIC)
 
-write_csv(CPE_data, "X:/2205_BKT_feral_broodstock_ID/Thometz_scripts/CPE_data_2205.csv")
+write_csv(CPE_data, "X:/filepath.../CPE_data_2205.csv")
